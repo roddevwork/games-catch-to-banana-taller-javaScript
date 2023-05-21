32ee7b1540b0dde727d8179df9183f1e8e5945e1
roddevwork <rodrigocardona87@gmail.com>
2023-05-21 17:07:11 -0300

    collision aded

--- src/game.js
+++ src/game.js
@@ -7,11 +7,20 @@ const btnLeft = document.querySelector("#btn-left")
 
 let canvasSize
 let elementSize
+let level = 0
+let lives = 3
 
 const playerPosition = {
   x: undefined,
   y: undefined
 }
+
+const giftPosition = {
+  x: undefined,
+  y: undefined
+}
+
+let enemyPositions = []
 
 window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
@@ -36,11 +45,18 @@ function startGame() {
   game.font = elementSize + 'px Verdana'
   game.textAlign = 'end'
 
-  const map = maps[0]
+  const map = maps[level]
+
+  if (!map) {
+    gameWin()
+    return;
+  }
+
   const mapRows = map.trim().split("\n")
   const mapRowsColumn = mapRows.map(row => row.trim().split(""))
   // console.log(map, mapRows, mapRowsColumn);
 
+  enemyPositions = []
   game.clearRect(0, 0, canvasSize, canvasSize)
   mapRowsColumn.forEach((row, rowI) => {
     row.forEach((col, colI) => {
@@ -54,6 +70,14 @@ function startGame() {
           playerPosition.y = posY
           console.log(playerPosition);
         }
+      } else if (col == 'I') {
+        giftPosition.x = posX
+        giftPosition.y = posY
+      } else if (col == 'X') {
+        enemyPositions.push({
+          x: posX,
+          y: posY
+        })
       }
 
       game.fillText(emoji, posX, posY)
@@ -64,7 +88,53 @@ function startGame() {
 }
 
 function movePLayer() {
+
+  const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3)
+  const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3)
+  const giftCollision = giftCollisionX && giftCollisionY
+
+
+  if (giftCollision) {
+    levelWin()
+  }
+
+  const enemyCollision = enemyPositions.find(enemy => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3)
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3)
+    return enemyCollisionX && enemyCollisionY
+  })
+
+  if (enemyCollision) {
+    levelFail()
+  }
+
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+}
+
+function levelWin() {
+  console.log("subiste de nivel");
+  level++
+  startGame()
+}
+
+function levelFail() {
+  lives--
+  console.log(lives);
+
+
+  if (lives <= 0) {
+    level = 0
+    lives = 3
+
+  }
+
+  playerPosition.x = undefined
+  playerPosition.y = undefined
+  startGame()
+}
+
+function gameWin() {
+  console.log("terminaste el juego");
 }
 
 window.addEventListener('keydown', moveByKeys)
@@ -116,5 +186,9 @@ function moveLeft() {
   } else {
     playerPosition.x -= elementSize
     startGame()
+  }
+  const giftPosition = {
+    x: undefined,
+    y: undefined
   }
 }
