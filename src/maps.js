c2ab69a20883b12df09ac62707255736ae3e301b
roddevwork <rodrigocardona87@gmail.com>
2023-05-25 20:35:29 -0300

    updated

--- src/maps.js
+++ src/maps.js
@@ -35,76 +35,76 @@ maps.push(`
   XXXX-----X
   XXXXXXXX-I
 //   `);
-// maps.push(`
-//   I-----XXXX
-//   XXXXX-XXXX
-//   XX----XXXX
-//   XX-XXXXXXX
-//   XX-----XXX
-//   XXXXXX-XXX
-//   XX-----XXX
-//   XX-XXXXXXX
-//   XX---X---X
-//   XXXX---X-O
-// `);
-// maps.push(`
-//   OXXXXXXXXX
-//   ---XXXXXXX
-//   XX--XXXXXX
-//   I-X--XXXXX
-//   X--X--XXXX
-//   XX--X--XXX
-//   XXX--X--XX
-//   XXXX--X--X
-//   XXXXX--X-X
-//   XXXXXX---X
-//   XXXXXXXXXX
-// `);
-// maps.push(`
-//   I--XX---XX
-//   XX----X--X
-//   XXXXXXXX--
-//   O---XXXXX-
-//   XXX-XXXX--
-//   ----XX---X
-//   -X-X--X-XX
-//   -X---XX-XX
-//   -XXXXXX-XX
-//   ---------X
-// `);
-// maps.push(`
-//   O---------
-//   XXXXXXXXX-
-//   --------X-
-//   -XXXXXX-X-
-//   -X----X-X-
-//   -X-XXIX-X-
-//   -X-XXXX-X-
-//   -X------X-
-//   -XXXXXXXX-
-//   ----------
-// `);
-// maps.push(`
-//   I---------
-//   XXXXXX-XX-
-//   --------X-
-//   -XXXXXX-X-
-//   -X----X-X-
-//   -X-XXOX-X-
-//   -X-XXXX-X-
-//   -X------X-
-//   -XXXXXXXX-
-//   ----------
-// `);
-// maps.push(`
-//   O---------
-//   XXXXXXXXX-
-//   I---X---X-
-//   -X--X---X-
-//   -X--X---X-
-//   -X--X---X-
-//   -X--X---X-
-//   --------X-
-//   -XX-X-X-X-
-//   --X---X---
-// `);
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX---X---X
+  XXXX---X-O
+`);
+maps.push(`
+  OXXXXXXXXX
+  ---XXXXXXX
+  XX--XXXXXX
+  I-X--XXXXX
+  X--X--XXXX
+  XX--X--XXX
+  XXX--X--XX
+  XXXX--X--X
+  XXXXX--X-X
+  XXXXXX---X
+  XXXXXXXXXX
+`);
+maps.push(`
+  I--XX---XX
+  XX----X--X
+  XXXXXXXX--
+  O---XXXXX-
+  XXX-XXXX--
+  ----XX---X
+  -X-X--X-XX
+  -X---XX-XX
+  -XXXXXX-XX
+  ---------X
+`);
+maps.push(`
+  O---------
+  XXXXXXXXX-
+  --------X-
+  -XXXXXX-X-
+  -X----X-X-
+  -X-XXIX-X-
+  -X-XXXX-X-
+  -X------X-
+  -XXXXXXXX-
+  ----------
+`);
+maps.push(`
+  I---------
+  XXXXXX-XX-
+  --------X-
+  -XXXXXX-X-
+  -X----X-X-
+  -X-XXOX-X-
+  -X-XXXX-X-
+  -X------X-
+  -XXXXXXXX-
+  ----------
+`);
+maps.push(`
+  O---------
+  XXXXXXXXX-
+  I---X---X-
+  -X--X---X-
+  -X--X---X-
+  -X--X---X-
+  -X--X---X-
+  --------X-
+  -XX-X-X-X-
+  --X---X---
+`);
